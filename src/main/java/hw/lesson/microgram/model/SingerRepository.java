0e15aa5418ec9eb0211f5b5f50a250389f2764fa
dzhaanbaeva <dzhaanbaeva@gmail.com>
2020-03-12 17:42:13 +0600

    add repository interfaces

---
+++ src/main/java/hw/lesson/microgram/model/SingerRepository.java
@@ -0,0 +1,6 @@
+package hw.lesson.microgram.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface SingerRepository extends CrudRepository<Singer, String> {
+}
---
+++ src/main/java/hw/lesson/microgram/model/TrackRepository.java
@@ -0,0 +1,6 @@
+package hw.lesson.microgram.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TrackRepository extends CrudRepository<Track, String> {
+}
